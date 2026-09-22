641af720be9f126bcc021a5d3e10de610b420641
Ruijie <pajarsultan2@gmail.com>
2026-09-22 20:46:34 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -14,7 +14,8 @@ local scripts = {
     splice10 = "https://raw.githubusercontent.com/ruijiecode/growtopia-script/refs/heads/main/splice/v1.0.lua",
     harvest421 = "https://raw.githubusercontent.com/ruijiecode/growtopia-script/refs/heads/main/harvest/v.4.2.1.lua",
     transfer10 = "https://raw.githubusercontent.com/ruijiecode/growtopia-script/refs/heads/main/transfer/v1.0.lua",
-    harvest422 = "https://raw.githubusercontent.com/ruijiecode/growtopia-script/refs/heads/main/harvest/v4.2.2.lua"
+    harvest422 = "https://raw.githubusercontent.com/ruijiecode/growtopia-script/refs/heads/main/harvest/v4.2.2.lua",
+    transfer11 = "https://raw.githubusercontent.com/ruijiecode/growtopia-script/refs/heads/main/transfer/v1.1.lua"
 }
 
 local function listKeys(t)
